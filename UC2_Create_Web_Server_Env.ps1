ad1d183cff15cb8a659d416b8f40c6e589fde1dd
CallumMSFT <callum.lowe@microsoft.com>
2017-10-07 09:33:34 +1300

    Updated UC2_Create_Web_Server_Env.ps1 in Azure Automation.

--- UC2_Create_Web_Server_Env.ps1
+++ UC2_Create_Web_Server_Env.ps1
@@ -1,10 +1,34 @@
- # create our new Azure Resource Group
-  New-AzureRmResourceGroup -Name "techhui" -Location "Southeast Asia"
+# Authentication  
+Write-Output ""
+Write-Output "------------------------ Authentication ------------------------"
+Write-Output "Logging in to Azure ..."
+
+$connectionName = "AzureRunAsConnection"
+
+# Get the connection "AzureRunAsConnection "
+$servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+$null = Add-AzureRmAccount `
+  -ServicePrincipal `
+  -TenantId $servicePrincipalConnection.TenantId `
+  -ApplicationId $servicePrincipalConnection.ApplicationId `
+  -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+
+Write-Output "Successfully logged in to Azure." 
+
+# create our new Azure Resource Group
+Write-Output ""
+Write-Output "------------------------ Creating resource group ------------------------"
+New-AzureRmResourceGroup -Name "techhui" -Location "Southeast Asia"
   
-  # create a new deployment by calling the arm template and parameters file off GitHub
-  New-AzureRmResourceGroupDeployment `
-    -Name techhui `
-    -ResourceGroupName techhui `
-    -TemplateUri https://raw.githubusercontent.com/CallumMSFT/calowe-hotmail/master/azuredeploy.json  `
-    -TemplateParameterUri https://raw.githubusercontent.com/CallumMSFT/calowe-hotmail/master/azuredeploy.parameters.json 
-    
+# create a new deployment by calling the arm template and parameters file off GitHub
+Write-Output ""
+Write-Output "------------------------ Creating deployment ------------------------"
+New-AzureRmResourceGroupDeployment `
+  -Name "techhui" `
+  -ResourceGroupName "techhui" `
+  -TemplateUri https://raw.githubusercontent.com/CallumMSFT/calowe-hotmail/master/azuredeploy.json  `
+  -TemplateParameterUri https://raw.githubusercontent.com/CallumMSFT/calowe-hotmail/master/azuredeploy.parameters.json
+
+
+
