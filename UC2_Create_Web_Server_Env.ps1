75e2a904d869e368fbb565874a5915ac7f0ba5c6
CallumMSFT <callum.lowe@microsoft.com>
2017-10-23 18:28:16 +1300

    Updated UC2_Create_Web_Server_Env.ps1 in Azure Automation.

--- UC2_Create_Web_Server_Env.ps1
+++ UC2_Create_Web_Server_Env.ps1
@@ -13,7 +13,7 @@ $null = Add-AzureRmAccount `
 $null = New-AzureRmResourceGroup -Name "techhui" -Location "Southeast Asia"
   
 # create a new deployment by calling the arm template and parameters file off GitHub
-$null = New-AzureRmResourceGroupDeployment `
+New-AzureRmResourceGroupDeployment `
   -Name "techhui" `
   -ResourceGroupName "techhui" `
   -TemplateUri https://raw.githubusercontent.com/CallumMSFT/calowe-hotmail/master/azuredeploy.json  `
